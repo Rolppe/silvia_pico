6c1b4cea9814557073515dd82f6df954ab08a1a9
Mikko Paaso <mikko@MacBook-Pro-2.local>
2025-12-08 17:46:53 +0200

    changed github update at startup under water switch

--- boot.py
+++ boot.py
@@ -2,11 +2,13 @@ import ugit
 from machine import Pin
 import time
 
+switch_water = Pin(8, Pin.IN, Pin.PULL_DOWN)
+
 time.sleep(0.1) # Power switch now is working bit slow method so giving some time
 
-switch_steam = Pin(9, Pin.IN, Pin.PULL_DOWN)
+#switch_steam = Pin(9, Pin.IN, Pin.PULL_DOWN)
 
-if not switch_steam.value():
+# If Water switch is on at the start, Check for updates from github
+if switch_water.value():
     ugit.pull_all()
     
-# ugit.pull_all()
